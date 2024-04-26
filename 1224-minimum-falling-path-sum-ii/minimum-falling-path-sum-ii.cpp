0014bcdc1f74cc66cf2146b5398bc83d303093c9
milind <112349965+milovate@users.noreply.github.com>
2024-04-26 22:17:10 +0530

    Time: 317 ms (34.31%) | Memory: 18.4 MB (16.18%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
-    int recur(int r, int i, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if(r==matrix.size()-1 && i<matrix[0].size() && i>=0){
-            return matrix[r][i];
+
+
+    int dfs(int row,int col,vector<vector<int>>& grid,vector<vector<int>>&dp){
+        int n=grid.size();
+        int m=grid[0].size();
+
+        if(row==n-1 && col<m && col>=0){
+            return grid[row][col];
         }
-        if(i>=matrix[0].size() || i<0) return INT_MAX;
-        if(dp[r][i]!=INT_MAX)return dp[r][i];
-        
+          
+        if(col>=m||col<0)return INT_MAX;
+        if(dp[row][col]!=INT_MAX)return dp[row][col];
+      
         int mini=INT_MAX;
-        for(int k=0;k<matrix[0].size();k++){
-            if(k!=i){
-                mini=min(mini, recur(r+1, k, matrix, dp));
+         for(int k=0;k<m;k++){
+            if(k!=col){
+                mini=min(mini, dfs(row+1, k,grid,dp));
             }
         }
-        
-        return dp[r][i] = matrix[r][i]+mini;
+
+        return dp[row][col] = grid[row][col]+mini;
+
     }
-    
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        vector<vector<int>> dp(rows, vector<int>(cols, INT_MAX));
-        
+
+
+
+    int minFallingPathSum(vector<vector<int>>& grid) {
         int mini=INT_MAX;        
-        for(int i=0;i<matrix[0].size();i++){
-            mini=min(recur(0, i, matrix, dp), mini);
+                int rows=grid.size();
+        int cols=grid[0].size();
+        vector<vector<int>> dp(rows, vector<int>(cols, INT_MAX));
+
+        for(int i=0;i<grid[0].size();i++){
+            mini=min(dfs(0, i, grid,dp), mini);
         }
         return mini;
     }
