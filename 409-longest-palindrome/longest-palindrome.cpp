10f8adbb1404223efcc2c7dfc4221d9f59d9f2a7
milind <112349965+milovate@users.noreply.github.com>
2024-06-04 17:15:54 +0530

    Time: 6 ms (16.33%) | Memory: 8.9 MB (48.94%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+      //if eve one is odd add 1 to final answer 
+      // freqency iff odd bool =true , freq-1 is added 
+      map<char,int> mp;
+      for(int i=0;i<s.size();i++){
+        mp[s[i]]++;
+      }
+      bool flg=false;
+      int ans=0;
+      for(auto it:mp){
+        if(it.second%2!=0){
+            flg=true;
+            ans+=it.second-1;
+        }
+        else ans+=it.second;
+
+      }
+
+if(flg)return ans+1;      
+return ans;
+    }
+};
