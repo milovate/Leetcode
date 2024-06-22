21b99ef452355df8c46484b90d88168e3fc6c458
milind <112349965+milovate@users.noreply.github.com>
2024-06-23 00:07:45 +0530

    Time: 27 ms (41.48%) | Memory: 35 MB (8.05%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -4,14 +4,15 @@ public:
         int n=customers.size();
         vector<int>pre(n,0);
         if(g[0]==1)pre[0]=customers[0];
-    cout<<pre[0]<<" ";
+    // cout<<pre[0]<<" ";
         for(int i=1;i<n;i++){
             if(g[i]==1){//cout<<i<<endl;
                 pre[i]=pre[i-1]+customers[i];
             }   
             else pre[i]=pre[i-1];
-            cout<<pre[i]<<" ";
-        }cout<<endl;
+            // cout<<pre[i]<<" ";
+        }
+        
 
         int sum=0;
         int maxi=pre[k-1];
@@ -22,12 +23,12 @@ public:
             if(k==0)break;
             if(pre[i]-pre[i-k]>maxi){
                 maxi=pre[i]-pre[i-k];
-                cout<<maxi<<endl;
+                // cout<<maxi<<endl;
                 ii=i-k+1;
                 jj=i;
             }
         }
-        cout<<ii <<jj;
+        // cout<<ii <<jj;
 
         int total=0;
         for(int i=0;i<n;i++){
