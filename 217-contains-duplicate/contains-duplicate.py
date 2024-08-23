5d87c3bac28be7232850050ce6425d21c325ff54
milind <112349965+milovate@users.noreply.github.com>
2024-08-24 00:50:48 +0530

    Time: 511 ms (5.16%) | Memory: 35.7 MB (15.57%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,8 +1,23 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = {}
+        # seen = {}
+        # for num in nums:
+        #     if num in seen and seen[num] >= 1:
+        #         return True
+        #     seen[num] = seen.get(num,0)+ 1
+        # print(seen)
+        # return False
+
+
+        d=collections.defaultdict(int)
+        ans=0
         for num in nums:
-            if num in seen and seen[num] >= 1:
+            d[num]+=1
+        
+        for key,value in d.items():
+            print(f"{key}  {value}")
+            if value>1:
+                ans+=1
                 return True
-            seen[num] = seen.get(num, 0) + 1
+        
         return False
