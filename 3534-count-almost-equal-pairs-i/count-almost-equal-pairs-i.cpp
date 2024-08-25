20bc870beeb3f8009fc3caee7c26248cb04f0968
milind <112349965+milovate@users.noreply.github.com>
2024-08-25 13:09:48 +0530

    Time: 1490 ms (9.09%) | Memory: 374.1 MB (18.18%) - LeetSync

---
+++ 3534-count-almost-equal-pairs-i/count-almost-equal-pairs-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+
+    bool check(string a,string b){
+        int cnt=0;
+        if(a==b)return true;
+        unordered_map<char,int>mpa,mpb;
+        for(int i=0;i<a.size();i++){
+            mpa[a[i]]++;
+            mpb[b[i]]++;
+        }
+        for(int i=0;i<a.size();i++){
+            if(a[i]!=b[i]&&mpa==mpb)cnt++;
+        }
+        cout<<a<<endl;
+        cout<<b<<" "<<cnt<<endl;
+        
+        return (cnt==2)?true:false;
+    }
+
+
+
+    int countPairs(vector<int>& nums) {
+
+        int n=nums.size();
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            v[i]+=(100000000+nums[i]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(check(to_string(v[i]),to_string(v[j]))){
+                    ans++;
+                }
+            }
+        }
+
+
+        return ans;
+        
+        
+    }
+};
