965a25f431313b190a5233ddc4b50858c87bb03d
milind <112349965+milovate@users.noreply.github.com>
2024-09-04 01:55:52 +0530

    Time: 1014 ms (5.00%) | Memory: 389.8 MB (5.05%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,27 +1,68 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
+        int n=s.size();
+        vector<int>v(n,0);
+
+        int seena=0;
+        stack<int>sa;
+        int total=0;
         if (x<y) {
             swap(x,y);
             reverse(s.begin(),s.end());
         }
-        int ans =0,acount =0,bcount= 0;
-        for (char c: s) {
-            if (c == 'a' ) acount++;
-            else if ( c =='b') {
-                if (acount >0 ) {
-                    --acount;
-                    ans += x;
-                } else {
-                    ++bcount;
-                }
-            } else {
-                ans += min(acount,bcount)*y;
-                acount =0;
-                bcount =0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='a'&& v[i]!=1){
+                seena++;
+                sa.push(i);
             }
+            else if(s[i]=='b'&&seena>0&& v[i]!=1){
+                total+=x;
+                seena--;
+                int t=sa.top();
+                sa.pop();
+                v[t]=1;
+                v[i]=1;
+            }
+            else if(v[i]!=1){
+                seena=0;
+                sa = stack<int>();
+            }
+
         }
-        ans += min(acount,bcount)*y;
-        return ans;
+        sa=stack<int>();
+        seena=0;
+
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='b'&& v[i]!=1){
+                seena++;
+                sa.push(i);
+            }
+            else if(s[i]=='a'&&seena>0&& v[i]!=1){
+                total+=y;
+                seena--;
+                int t=sa.top();
+                sa.pop();
+                v[t]=1;
+                v[i]=1;
+            }
+            else if (v[i]!=1){
+                seena=0;
+                sa = stack<int>();
+            }
+
+        }
+
+
+
+
+        return total;
+
+
+
+        
+        
     }
 };
