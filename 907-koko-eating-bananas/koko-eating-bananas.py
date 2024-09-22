0edab6ac17d8b01921cbbe7f16c14874ed95c0d2
milind <112349965+milovate@users.noreply.github.com>
2024-09-22 22:10:47 +0530

    Time: 305 ms (19.23%) | Memory: 18.2 MB (58.22%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,36 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+
+
+        def checkPossible(node) -> bool:
+            hrsTaken=0
+            for x in piles:
+                hrsTaken+=math.ceil(x/node)
+            # print(hrsTaken)
+            return hrsTaken<=h
+        
+        
+
+        left=1
+        right=10000000000
+
+        while(left<=right):
+            mid=left+(right-left)//2
+
+            # print(mid)
+            # print(checkPossible(mid))
+
+            if checkPossible(mid):
+                right=mid-1
+            else :
+                left=mid+1
+        
+
+        return left
+
+
+
+
+        
