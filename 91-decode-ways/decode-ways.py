b715134f645369296c8b37a213a85b700a034578
milind <112349965+milovate@users.noreply.github.com>
2024-10-07 19:47:50 +0530

    Time: 38 ms (43.60%) | Memory: 16.7 MB (32.14%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,29 +1,63 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # Initialize
+        memo = {}
 
         
-        #  taken till 2 digits and less than 26
-        if s[0]=='0':
-            return 0
-        n=len(s)
+        def find_ways(index):
 
-        dp=[0]*(n+1)
-
-        dp[0]=1
-        dp[1]=1
-
-
-        for i in range(2,n+1,1):
-        
-            one=int(s[i-1])
-            if one >=1 and one<=9:
-                dp[i]+=dp[i-1]
+            # Check if done
+            if index == len(s):
+                return 1
+            # Check if invalid
+            elif s[index] == '0':
+                return 0
+            # Check if last digit
+            elif index == len(s) - 1:
+                return 1
+            # CHeck if visited before
+            elif index in memo:
+                return memo[index]
             
-            ten=int(s[i-2:i])
+            # Add 1 way
+            ways = find_ways(index + 1)
+            # Potentially add second way
+            if int(s[index:index + 2]) <= 26:
+                ways += find_ways(index + 2)
+            
+            # Save and return
+            memo[index] = ways
+            return ways
+        
+        # Start recursion
+        return find_ways(0)
 
-            if ten>=10 and ten<=26:
-                dp[i]+=dp[i-2]
+# class Solution:
+#     def numDecodings(self, s: str) -> int:
 
-            # print(one,ten)
+        
+#         #  taken till 2 digits and less than 26
+#         if s[0]=='0':
+#             return 0
+#         n=len(s)
 
-        return dp[n]
+#         dp=[0]*(n+1)
+
+#         dp[0]=1
+#         dp[1]=1
+
+
+#         for i in range(2,n+1,1):
+        
+#             one=int(s[i-1])
+#             if one >=1 and one<=9:
+#                 dp[i]+=dp[i-1]
+            
+#             ten=int(s[i-2:i])
+
+#             if ten>=10 and ten<=26:
+#                 dp[i]+=dp[i-2]
+
+#             # print(one,ten)
+
+#         return dp[n]
